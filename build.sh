94c49a17b3416fddbe04d1d83510bccccf28bc78
Piecuch Pawel <piecuch.pawel@gmail.com>
2024-08-08 17:48:27 +0200

    update rpi build issues

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -e
 [ -d obj ] && rm -r obj
 [ ! -d obj ] && mkdir obj
 
-OPTS="-Wno-sign-compare"
+OPTS="-Wno-sign-compare -Wno-deprecated-declarations"
 
-./bin/gcc-wrap $OPTS --outdir=obj -c -I include figlet_font/*.cpp text_ui/*.c graphics/*.c
+./bin/gcc-wrap $OPTS --outdir=obj -c -I include figlet_font/*.cpp text_ui/*.c graphics/*.c main.cpp
