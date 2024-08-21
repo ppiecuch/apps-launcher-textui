2b57c333fc7d578259cebf4a4719c313b42e4f5e
Pawel Piecuch <piecuch.pawel@gmail.com>
2024-08-21 11:42:01 +0200

    build update

--- main.h
+++ main.h
@@ -5,22 +5,28 @@
 
 /* Low-level frambuffer management */
 
-int fb_acquire_fb(uint32_t flags, const char *fb_device, const char *tty_device);
-void fb_release_fb(void);
+#ifdef __cplusplus
+# define EXTERN_C extern "C"
+#else
+# define EXTERN_C
+#endif
 
-uint32_t fb_screen_width(void);
-uint32_t fb_screen_height(void);
+EXTERN_C int fb_acquire_fb(uint32_t flags, const char *fb_device, const char *tty_device);
+EXTERN_C void fb_release_fb(void);
 
-uint32_t fb_console_width(void);
-uint32_t fb_console_height(void);
+EXTERN_C uint32_t fb_screen_width(void);
+EXTERN_C uint32_t fb_screen_height(void);
 
-void fb_flush_rect(int x, int y, int w, int h);
-void fb_flush_window(void);
-int fb_flush_fb(void);
+EXTERN_C uint32_t fb_console_width(void);
+EXTERN_C uint32_t fb_console_height(void);
+
+EXTERN_C void fb_flush_rect(int x, int y, int w, int h);
+EXTERN_C void fb_flush_window(void);
+EXTERN_C int fb_flush_fb(void);
 
 /* Intialization and termination */
 
-void alert(const char *msg);
-void fatal(const char *msg);
+EXTERN_C void alert(const char *msg);
+EXTERN_C void fatal(const char *msg);
 
 #endif // MAIN_H
